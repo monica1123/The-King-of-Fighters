669cbce2c8cd404831c1f48ba8c87ba7184acaf8
monica1123 <shelly1123@hotmail.com>
2021-04-25 22:45:52 +0800

    change name of ImageView

--- The King of Fighters/ViewController.swift
+++ The King of Fighters/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boy: UIImageView!
+    @IBOutlet weak var girl: UIImageView!
+    @IBOutlet weak var fire: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var boy2: UIImageView!
+    @IBOutlet weak var smallboy2: UIImageView!
+    
+    var degrees: Float = 0
+    
+    func updateLocation() {
+        
+         slider.value = degrees
+         boy2.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi / -180 * 2 * CGFloat(degrees)).translatedBy(x: -180, y: 120)
+        
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func moveSlider(_ sender: UISlider) {
+        
+        let speed = Int(sender.value * 3)
+        boy.frame.origin.x = CGFloat(-5 + speed * 5)
+        girl.frame.origin.x = CGFloat(375 - speed * 3)
+       
+        boy2.alpha = CGFloat(sender.value/10)
+        smallboy2.alpha = CGFloat(sender.value/10)
+        fire.alpha = CGFloat(sender.value/10)
+        
+        degrees = sender.value
+        updateLocation()
+    
+    }
+    
 }
 
